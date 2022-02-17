f77e8935b4a81c12f6556c59e4a73e44a2434c6e
shauryapuri97 <puri.shaurya9@gmail.com>
2022-02-17 01:18:16 +0000

    get trading pairs

--- orderbook/src/App.js
+++ orderbook/src/App.js
@@ -1,8 +1,28 @@
+import React, { useState, useEffect } from 'react';
 import { TradingPanel } from './components/Tradingpanel';
 import { OrderBook } from './components/OrderBook';
 import './App.css';
 
 function App() {
+
+  const [products, setProducts ] = useState([]);
+
+  useEffect(()=>{
+    const headers = {'Accept': 'application/json'}
+    try {
+      fetch('https://api-public.sandbox.exchange.coinbase.com/products', {headers})
+        .then(res => res.json())
+        .then(data => setProducts(data))
+    } catch (error) {
+      console.log("Failed to GET Products ");
+      console.log(error);
+    }
+  },[])
+
+  useEffect(()=>{
+    //Populate the search field
+  },[products])
+
   return (
     <div className="App">
       <div className="App-Header">
