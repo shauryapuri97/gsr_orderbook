b8741f3e55a74f7e49d182daa24a38167cad8f3f
shauryapuri97 <puri.shaurya9@gmail.com>
2022-02-17 15:17:17 +0000

    Added test case

--- orderbook/src/App.test.js
+++ orderbook/src/App.test.js
@@ -1,8 +1,22 @@
 import { render, screen } from '@testing-library/react';
-import App from './App';
+import App, { handleChange } from './App';
+import MemoziedOrderBook, { objToArr } from './components/OrderBook'
 
-test('renders learn react link', () => {
-  render(<App />);
-  const linkElement = screen.getByText(/learn react/i);
-  expect(linkElement).toBeInTheDocument();
+test('Should show header', () => {
+  render(<App />)
+  const input = screen.getByText('GSR Crypto Trading');
+  expect(input).toBeInTheDocument();
 });
+
+test('Should show trading pair selector label', () => {
+  render(<App />)
+  const input = screen.getByText('Select a Product');
+  expect(input).toBeInTheDocument();
+});
+
+test('Should show trading coming soon container', () => {
+  render(<App />)
+  const input = screen.getByText('Trading Chart');
+  expect(input).toBeInTheDocument();
+});
+
